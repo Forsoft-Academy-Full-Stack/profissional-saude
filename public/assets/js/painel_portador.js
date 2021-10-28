7535502f62e5696e12240a5a99998a662de4a267
randelsouza <randelsouza88@gmail.com>
2021-10-28 14:52:14 -0300

    Adicionando validacao em meus-dados

---
+++ public/assets/js/painel_portador.js
@@ -0,0 +1,12 @@
+tratar_campos = function () {
+    let erro = false;
+    let campos = ["nome", "sobrenome", "datanascimento", "documento", "nacionalidade", "codigociva", "codigopostal", "logradouro", "numerorua", "complemento", "bairro", "cidade", "estado", "contato", "email"];
+
+    for (i = 0; i < campos.length; i++) {
+        if (document.getElementById(campos[i]).value == '') {
+            erro = true;
+        }
+    }
+    return !erro;
+
+}
