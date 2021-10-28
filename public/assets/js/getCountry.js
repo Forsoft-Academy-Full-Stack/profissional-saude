028e9f82e2336b733d6ec35a512218578f9578c9
randelsouza <randelsouza88@gmail.com>
2021-10-28 09:12:45 -0300

    adicionado condicional ao buscar cep para nao gerar erro ao dar load na pagina e quando o cep e vazio

--- public/assets/js/getCountry.js
+++ public/assets/js/getCountry.js
@@ -2,14 +2,18 @@
     async () => {
         // PEGAR DADOS CEP
         const paises = await axios('./../public/assets/js/json/CountryCodes.json')
-        .then(nameCountry => nameCountry.data).catch(erro=> window.alert('Houve um erro ao carregar a API paises!'))
+            .then(nameCountry => { nameCountry.data })
+            .catch(erro => window.alert('Houve um erro ao carregar a API paises!'));
 
-        // EXIBIR NO INPUT NACIONALIDADE
-        for(let x = 0; x < paises.length; x++){
-            const option = document.createElement('option')
-            option.value = paises[x].code
-            option.innerText = paises[x].name
-            getElement('#nacionalidade').appendChild(option)
+
+        if (paises) {
+            // EXIBIR NO INPUT NACIONALIDADE
+            for (let x = 0; x < paises.length; x++) {
+                const option = document.createElement('option');
+                option.value = paises[x].code;
+                option.innerText = paises[x].name;
+                getElement('#nacionalidade').appendChild(option);
+            }
         }
     }
 )()
--- public/assets/js/searchCep.js
+++ public/assets/js/searchCep.js
@@ -1,21 +1,26 @@
 // PEGA ELEMENTOS HTML
-function getElement(id){ return document.querySelector(id) }
+function getElement(id) { return document.querySelector(id) }
 
 // INPUT CEP
-getElement('#codigopostal').addEventListener('change', searchCep)
+getElement('#codigopostal').addEventListener('focusout', searchCep);
 
-async function searchCep(event){
+async function searchCep(event) {
 
-const cepValue = event.target.value
+    const cepValue = event.target.value;
+    
+    // BUSCA API VIACEP
+    if(cepValue){
+        const cepResponse = await axios.get(`https://viacep.com.br/ws/${cepValue}/json/`)
+        .then(cep => { return cep.data })
+        .catch(erro => window.alert('CEP não encrontrado'));
 
-// BUSCA API VIACEP
-const cepResponse = await axios.get(`https://viacep.com.br/ws/${cepValue}/json/`)
-.then(cep => { return cep.data }).catch( erro => window.alert('CEP não encrontrado'))
+    // DEFINE VALORES AOS INPUTS
+    getElement('#logradouro').value = cepResponse.logradouro
+    getElement('#cidade').value = cepResponse.localidade
+    getElement('#bairro').value = cepResponse.bairro
+    getElement('#estado').value = cepResponse.uf
 
-// DEFINE VALORES AOS INPUTS
-getElement('#logradouro').value = cepResponse.logradouro
-getElement('#cidade').value = cepResponse.localidade
-getElement('#bairro').value = cepResponse.bairro
-getElement('#estado').value = cepResponse.uf
+    }
+ 
 
 }
