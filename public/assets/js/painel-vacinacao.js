ad916f304561776cd0cef74ed69bd72d16c5a496
randelsouza <randelsouza88@gmail.com>
2021-10-30 00:56:49 -0300

    compo data de nascimento e identidade adicionado por thailane

--- public/assets/js/painel-vacinacao.js
+++ public/assets/js/painel-vacinacao.js
@@ -1,6 +1,6 @@
 tratar_campos = function () {
     let erro = false;
-    let campos = ["name", "surname", "idvacinacao", "vacina", "laboratorio", "dose", "dataaplicacao", "pais", "idunidade", "unidade", "codigociva", "lote"];
+    let campos = ["name", "surname", "date-birth", "identity", "idvacinacao", "vacina", "laboratorio", "dose", "dataaplicacao", "pais", "idunidade", "unidade", "codigociva", "lote"];
 
     for (i = 0; i < campos.length; i++) {
         if (document.getElementById(campos[i]).value == '') {
